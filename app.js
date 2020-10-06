f9c2a864fd98fc36c1d51ff60a015a34e8e8e024
abduallahnoorsayket <abdullahnoorsayket@gmail.com>
2020-10-06 18:40:18 +0600

    watchers basics

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
-Vue.createApp({
+{ /* < script src = "https://cdn.jsdelivr.net/npm/axios@0.12.0/dist/axios.min.js" > < /script> */ }
+
+{ /* <script src = "https://cdn.jsdelivr.net/npm/axios@0.12.0/dist/axios.min.js" > </script> */ }
+const watchExampleVM = Vue.createApp({
     data() {
         return {
-            author: {
-                name: 'John Doe',
-                books: ['Vue 2 - Advanced Guide',
-                    'Vue 3 - Basic Guide',
-                    'Vue 4 - The Mystery'
-                ]
+            question: '',
+            answer: 'Questions usually contain a question mark. ;-)'
+        }
+    },
+    watch: {
+        // whenever question changes, this function will run
+        question(newQuestion, oldQuestion) {
+            if (newQuestion.indexOf('?') > -1) {
+                this.getAnswer()
             }
         }
     },
-    computed: {
-        // a computed getter
-        publishedBooksMessage() {
-            // `this` points to the vm instance
-            return this.author.books.length > 0 ? 'Yes' : 'No'
+    methods: {
+        getAnswer() {
+            this.answer = 'Thinking...'
+            axios
+                .get('https://yesno.wtf/api')
+                .then(response => {
+                    this.answer = response.data.answer
+                })
+                .catch(error => {
+                    this.answer = 'Error! Could not reach the API. ' + error
+                })
         }
     }
-}).mount('#computed-basics')
+}).mount('#watch-example')
