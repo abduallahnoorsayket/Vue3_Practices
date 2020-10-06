8f6da2fc04a7bfc43c88177ff3318749335121a2
abduallahnoorsayket <abdullahnoorsayket@gmail.com>
2020-10-06 18:38:49 +0600

    computed properties basics

--- app.js
+++ app.js
@@ -1,40 +1,20 @@
-// new Vue({
-//     el: '#app',
-//     data: {
-//         title: 'Becoming a vue ninja Abdulla sayket '
-//     }
-
-// })
-
-
-const app = Vue.createApp({});
-
-app.component('modal-button', {
-    template: `
-    <button @click="modalOpen = true">
-        Open full screen modal! (With teleport!)
-    </button>
-
-    <teleport to=".hello">
-      <div v-if="modalOpen" class="modal">
-        <div>
-          I'm a teleported modal! 
-          (My parent is "body")
-          hello
-
-          <button @click="modalOpen = false">
-            Close
-          </button>
-        </div>
-      </div>
-    </teleport>
-    
-  `,
+Vue.createApp({
     data() {
         return {
-            modalOpen: false
+            author: {
+                name: 'John Doe',
+                books: ['Vue 2 - Advanced Guide',
+                    'Vue 3 - Basic Guide',
+                    'Vue 4 - The Mystery'
+                ]
+            }
+        }
+    },
+    computed: {
+        // a computed getter
+        publishedBooksMessage() {
+            // `this` points to the vm instance
+            return this.author.books.length > 0 ? 'Yes' : 'No'
         }
     }
-})
-
-app.mount('#app')
+}).mount('#computed-basics')
